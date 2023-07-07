7f262880d7f52923a35f2bf24fd88f2436169487
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,5 @@
-function threeSum(arr, target) {
-// write your code here
-    let difference=Number.MAX_VALUE;
-    let result=0;
-    let n=arr.length;
-
-    // Now sort the array
-    arr.sort();
-
-    // Now form triplets
-    for(let i=0; i<n; i++)
-    {
-        let j=i+1;
-        let k=n-1;   // two pointers
-        while(j<k)
-        {
-            let sum=arr[i]+arr[j]+arr[k];
-            if(Math.abs(sum-target) < Math.abs(difference))
-            {
-                difference=sum-target;
-                result=sum;    // we need to return sum of triplets
-            }
-
-            // To update pointers
-            if(sum<target)
-            {
-                // Increase smaller value
-                j++;
-            }
-            else
-            {
-                // decrease larger value
-                k--;
-            }
-        }
-    }
-    return result;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
